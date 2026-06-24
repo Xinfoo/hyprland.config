ac0f68aaef74f0345859fbf8d6826475c016949e
Xinfoo <13994062125@163.com>
2026-06-24 14:56:51 +0800

    更改环境变量配置目录。

--- XDG_CONFIG_HOME/hypr/lua/environment.lua
+++ XDG_CONFIG_HOME/hypr/lua/environment.lua
@@ -2,6 +2,7 @@
 ---- 环境变量 ----
 ------------------
 
+-- 使用 uwsm 时，环境变量配置目录应在 $XDG_CONFIG_HOME/.config/uwsm/env 
 -- 参见 https://wiki.hypr.land/Configuring/Advanced-and-Cool/Environment-variables/
-hl.env("XCURSOR_SIZE", "24")
-hl.env("HYPRCURSOR_SIZE", "24")
+-- hl.env("XCURSOR_SIZE", "24")
+-- hl.env("HYPRCURSOR_SIZE", "24")

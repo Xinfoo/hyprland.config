582b4d2dde91e8b11ac9f2826dacfba62715156e
Xinfoo <13994062125@163.com>
2026-06-26 10:24:29 +0800

    增加触控板滚动设置。

--- XDG_CONFIG_HOME/hypr/lua/input.lua
+++ XDG_CONFIG_HOME/hypr/lua/input.lua
@@ -16,6 +16,7 @@ hl.config({
 
         touchpad = {
             natural_scroll = true,
+            scroll_factor  = 1.0,
         },
     },
 })
@@ -26,9 +27,15 @@ hl.gesture({
     action = "workspace"
 })
 
--- 示例：按设备配置
+-- 按设备配置
+-- scroll_factor 小于 1 会缩短滚动距离，大于 1 会放大滚动距离。
 -- 更多信息参见 https://wiki.hypr.land/Configuring/Advanced-and-Cool/Devices/
 hl.device({
-    name        = "epic-mouse-v1",
-    sensitivity = -0.5,
+    name          = "elan0676:00-04f3:3195-touchpad",
+    scroll_factor = 1.0,
+})
+
+hl.device({
+    name          = "tpps/2-elan-trackpoint",
+    scroll_factor = 1.0,
 })
